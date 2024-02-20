4a17aac2e4a9d767063204637ef7c4f99ba3ead3
Rayssa Ellen <65548341+ray396@users.noreply.github.com>
2024-02-20 15:37:18 -0300

    dia 9

--- RPA.py
+++ RPA.py
@@ -61,3 +61,72 @@ elif opcao == 'Notepad':
     escolha_opcao.press('Enter')
     escolha_opcao.sleep(2)
     escolha_opcao.typewrite('Escolhi abrir o Notepad')
+
+
+from selenium import webdriver
+
+abrirNavegador = webdriver.Chrome()
+abrirNavegador.get("https://www.google.com.br/")
+
+
+"""
+Extraindo Dólar e Euro da internet
+"""
+from selenium import webdriver as opcoes_selenium
+from selenium.webdriver.common.keys import Keys
+import pyautogui as tempoPausaComputador
+import pyautogui as teclasAtalhos
+import xlsxwriter
+import os
+
+from selenium.webdriver.common.by import By
+
+meuNavegador = opcoes_selenium.Chrome()
+meuNavegador.get("https://www.google.com.br/")
+tempoPausaComputador.sleep(2)
+
+meuNavegador.find_element(By.NAME, "q").send_keys("Dolar hoje")
+tempoPausaComputador.sleep(2)
+
+meuNavegador.find_element(By.NAME, "q").send_keys(Keys.RETURN)
+tempoPausaComputador.sleep(4)
+
+valorDolar = meuNavegador.find_elements(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')[0].text
+print(valorDolar)
+
+tempoPausaComputador.sleep(2)
+
+meuNavegador.find_element(By.NAME, "q").send_keys("")
+tempoPausaComputador.sleep(2)
+
+teclasAtalhos.press('tab')
+
+tempoPausaComputador.sleep(4)
+
+teclasAtalhos.press('enter')
+
+tempoPausaComputador.sleep(2)
+
+meuNavegador.find_element(By.NAME, "q").send_keys("Euro hoje")
+tempoPausaComputador.sleep(2)
+
+meuNavegador.find_element(By.NAME, "q").send_keys(Keys.RETURN)
+tempoPausaComputador.sleep(4)
+
+valorEuro = meuNavegador.find_elements(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')[0].text
+print(valorEuro)
+
+tempoPausaComputador.sleep(6)
+
+caminhoArquivo = "C:/Users/Tecnologia/Documents/GitHub/RPA/dolar_e_euro.xlsx"
+planilhaCriada = xlsxwriter.Workbook(caminhoArquivo)
+sheet1 = planilhaCriada.add_worksheet()
+
+sheet1.write("A1", "Dolar")
+sheet1.write("B1", "Euro")
+sheet1.write("A2", valorDolar)
+sheet1.write("B2", valorEuro)
+
+planilhaCriada.close()
+
+os.startfile(caminhoArquivo)
--- index.py
+++ index.py
@@ -1,3 +1,26 @@
 """
-Extraindo Dólar e Euro da internet
+Extraindo endereço pelos sites dos correios e salvando no excel
 """
+from selenium import webdriver as opcoesSelenium
+from selenium.webdriver.common.keys import Keys
+import pyautogui as tempoEspera
+
+from selenium.webdriver.common.by import By
+
+navegador = opcoesSelenium.Chrome()
+navegador.get("https://buscacepinter.correios.com.br/app/endereco/index.php")
+tempoEspera.sleep(3)
+
+navegador.find_element(By.NAME, "endereco").send_keys("05892387")
+tempoEspera.sleep(2)
+navegador.find_element(By.NAME, "btn_pesquisar").click()
+tempoEspera.sleep(3)
+
+rua = navegador.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[1]')[0].text
+print(rua)
+bairro = navegador.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[2]')[0].text
+print(bairro)
+cidade = navegador.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[3]')[0].text
+print(cidade)
+cep = navegador.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[4]')[0].text
+print(cep)
